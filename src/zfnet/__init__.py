764e3f9fbdd5f2620df6bfe15a5a447218b0a32f
piercemullen <45112367+pnm4sfix@users.noreply.github.com>
2025-07-28 13:24:51 +0100

    Updated _init.py

--- src/zfnet/__init__.py
+++ src/zfnet/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Pierce Mullen"""
 __email__ = 'pnm1@st-andrews.ac.uk'
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 from .io import read_zarr, extract_activity
 from .conn import find_peaks_in_signal, compute_correlation
